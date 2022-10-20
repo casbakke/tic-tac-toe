28d9ca91c249a061d853161861969489f1910035
Craig Bakke <casbakke@gmail.com>
2022-10-20 08:24:37 -0400

    Added lots of comments

--- main.py
+++ main.py
@@ -1,36 +1,37 @@
-import tkinter as tk
-from random import randint
+import tkinter as tk # for graphical interface
+from random import randint # for randomized player selection
 
 font0 = ("Arial", 48)
 font1 = ("Arial", 64)
 
-window = tk.Tk()
+window = tk.Tk() # main window object
 window.title("Tic-Tac-Toe")
-window.geometry("600x700+100+50")
-window.maxsize(600, 700)
+window.geometry("600x700+100+50") # entire widow will be 600x700px, opens at coords (100, 50)
+window.maxsize(600, 700) # lock window to a fixed size
 window.minsize(600, 700)
 
-window.rowconfigure(0, minsize = 100)
-window.rowconfigure(1, minsize = 600)
+window.rowconfigure(0, minsize = 100) # header will be 100px tall
+window.rowconfigure(1, minsize = 600) # body will be 600x600px
 
-player = "X"
-stopped = False
+player = "X" # first player upon launch is always X
+stopped = False # this allows the buttons to be used
 
-frm_head = tk.Frame(
+frm_head = tk.Frame( # header frame containing the status widget
     window,
     relief = tk.FLAT,
 )
 
-frm_board = tk.Frame(
+frm_board = tk.Frame( # board frame containing the 9 button widgets
     window,
     relief = tk.FLAT,
 )
 
+# button widgets
 btn_A1 = tk.Button(
-    frm_board,
-    font = font1,
-    borderwidth = 5,
-    command = lambda: button_clicked("A1")
+    frm_board, # button container
+    font = font1, # text font
+    borderwidth = 5, # button border width
+    command = lambda: button_clicked("A1") # calls button_clicked("A1") upon click
 )
 
 btn_A2 = tk.Button(
@@ -89,12 +90,13 @@ btn_C3 = tk.Button(
     command = lambda: button_clicked("C3")
 )
 
-lbl_status = tk.Label(
+lbl_status = tk.Label( # status label at the top of the screen
     frm_head,
     text = f"{player}'s Turn",
     font = font0,
 )
 
+# display widgets
 frm_board.rowconfigure((0,1,2), minsize = 200)
 frm_board.columnconfigure((0,1,2), minsize = 200)
 
@@ -128,11 +130,12 @@ def button_clicked(ident): # when a button is clicked
             else: # instead continue playing
                 player = "X" if (player == "O") else "O"
                 lbl_status["text"] = f"{player}'s Turn"
-    else:
-        for btn in frm_board.winfo_children():
-            btn["text"] = ""
+
+    else: # if the game is stopped, reset the board
+        for btn in frm_board.winfo_children(): # get all buttons in the window
+            btn["text"] = "" # set button text to null
         players = ["X", "O"]
-        player = players[randint(0,1)]
+        player = players[randint(0,1)] # choose random player
         lbl_status["text"] = f"{player}'s Turn"
         stopped = False
 
@@ -142,19 +145,19 @@ def check_win(): # determine if the game has finished, returns T/F
 
 def check_draw(): # determine if the game board has been filled up with no winner
     str = ""
-    for tile in frm_board.winfo_children():
-        str = str + tile["text"]
-    if len(str) != 9:
-        return False
+    for tile in frm_board.winfo_children(): # get each tile on the board
+        str = str + tile["text"] # concatenate tile's text to a temporary string
+    if len(str) != 9: # if the string does not have 9 values
+        return False # continue the game
     else:
-        return True
+        return True # announce a draw
 
-def won(winner):
+def won(winner): # stop the game and announce the winner
     global stopped
-    stopped = True
+    stopped = True # disables the game buttons
     lbl_status["text"] = f"{winner} Wins!"
 
-def draw():
+def draw(): # stop the game and announce a draw
     global stopped
     stopped = True
     lbl_status["text"] = "Draw!"
